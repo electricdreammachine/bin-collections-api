5a08a7e32b353bbfb7cc937fc24270491e4eacdd
Ollie <hello@olliesmith.dev>
2020-03-29 16:32:10 +0100

    dont fatal error if env not read

--- internal/pkg/get-config-value/get-config-value.go
+++ internal/pkg/get-config-value/get-config-value.go
@@ -1,16 +1,12 @@
 package getconfigvalue
 
 import (
-	"log"
 	"github.com/joho/godotenv"
 )
 
 // ByKey uses godot package to load/read the .env file and return value for key
 func ByKey(key string) string {
-	configMap, err := godotenv.Read(".env.yml")
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
+	configMap,_ := godotenv.Read(".env.yml")
 
 	return configMap[key]
 }
