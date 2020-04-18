a1ff9cfc19b0ad77b66b9d5a81fe41b5240c51bf
Ollie <hello@olliesmith.dev>
2020-04-18 15:32:13 +0100

    parameterise collections endpoint

--- internal/collections-for-address/get-collections-for-address.go
+++ internal/collections-for-address/get-collections-for-address.go
@@ -1,22 +1,27 @@
 package getcollectiondates
 
 import (
-	"fmt"
 	"net/http"
+	"log"
 	"encoding/json"
 	"bin-collections-api/internal/pkg/get-tokens"
 )
 
+type collectionSearchRequest struct {
+	addressID string
+}
+
 // GetCollectionsForID blocks until tokens retrieved and then retrieves collections for the given ID
 func GetCollectionsForID(w http.ResponseWriter, request *http.Request) {
-	//TODO: get id from request body
+	var collectionSearch collectionSearchRequest
+	err := json.NewDecoder(request.Body).Decode(&collectionSearch)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	instanceTokens := <-gettokens.GetTokens()
-	collectionDates := <-ForUniqueAddressID(instanceTokens, "250056153")
-
-	// fmt.Println(collectionTypes)
-
-	//TODO: construct http response
-	fmt.Println(collectionDates)
+	collectionDates := <-ForUniqueAddressID(instanceTokens, collectionSearch.addressID)
 
 	json.NewEncoder(w).Encode(collectionDates)
 }
